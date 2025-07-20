c87d5a459cab5e07e6216f0aceaf79fbf99d7054
berkay <berkay_berber17@hotmail.com>
2025-07-20 15:50:14 +0300

    Data varibles , names rules

---
+++ src/main/java/com/berkayberber/_1_part_javatype/_2_Variables.java
@@ -0,0 +1,25 @@
+package com.berkayberber._1_part_javatype;
+
+public class _2_Variables {
+    public static void main(String[] args) {
+        // Veri değişken isimleri yazarken ;
+        /*
+            veri isimlendirmeleri;
+            1- isim veya sıfat,  zamir kullanmamız lazım.
+            2- _ veya $ ile başlayabiliriz
+            3- sayı ile bitebilir
+            4- sayı ile başlamaz
+            5- _ veya $ dışında özel simgelerle başlanmaz
+            6- değişken isimlerini camelCase kuralına göre yazılır
+         */
+        // Primitive Types 8 tanedir. null değer alınamaz
+        //1- isim veya sıfat,  zamir kullanmamız lazım.
+
+        int schoolNumber = 25;
+        System.out.println("schoolNumber = " + schoolNumber);
+        schoolNumber = 344;
+        System.out.println("schoolNumber = " + schoolNumber);
+
+
+    }
+}
