d1448efb279d4eb1069ff9da75828582a3aca01e
berkay <berkay_berber17@hotmail.com>
2025-07-20 18:40:15 +0300

    Operators

---
+++ src/main/java/com/berkayberber/_1_part_javatype/_04_Operators.java
@@ -0,0 +1,32 @@
+package com.berkayberber._1_part_javatype;
+
+public class _04_Operators  {
+    public static void main(String[] args) {
+        // Operatorler
+        // +
+        // -
+        // *
+        // /
+        // %
+        // ++
+        // --
+
+        int number1=23;
+        int number2=7;
+        int addition= number1+number2;
+        int subtraction = number1-number2;
+        int multiplication= number1*number2;
+        int division= number1/number2;
+        int modulus= number1%number2;
+
+        System.out.println("Toplam " + addition);
+        System.out.println("Çıkarma " + subtraction);
+        System.out.println("Çarpma " + multiplication);
+        System.out.println("Bölme " + division);
+        System.out.println("Kalan " +  modulus);
+
+        // MANTIKSAL OPERATORLER
+        // && = VE , || = VEYA , ! = DEĞİL
+        // ++ ARTTIR -- AZALTIR
+    }
+}
