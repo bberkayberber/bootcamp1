560fabe16737faa63b1fa8e7d1e6dee332efbcd0
berkay <berkay_berber17@hotmail.com>
2025-08-11 23:29:02 +0300

    conditionals

---
+++ src/main/java/com/berkayberber/_2_week/_09_02_Conditional.java
@@ -0,0 +1,18 @@
+package com.berkayberber._2_week;
+
+public class _09_02_Conditional {
+
+    public static void main(String[] args) {
+
+        int number=5;
+        if(number>0){
+            System.out.println("pozitif sayidir");
+        }
+        else{
+            System.out.println("negatif sayidir");
+        }
+        // YADA
+        String result = (number>0) ? "pozitif sayidir" : "negatif sayidir";
+        System.out.println(result);
+    }
+}
